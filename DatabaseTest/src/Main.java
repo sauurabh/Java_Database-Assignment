7aa740a3d0500f96bd0b0fec6e4cb1a490d5deff
Saurabh Kr <77803374+sauurabh@users.noreply.github.com>
2023-04-12 22:28:14 +0530

    Update Main.java

--- DatabaseTest/src/Main.java
+++ DatabaseTest/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println("Connected to SQL Server");
 
             //query for Hour of the day when the call volume is highest.
-            String query = "SELECT hour(start_time) AS HourOfDay,count(duration) AS TotalDuration from testData group By HourOfDay order By TotalDuration desc limit 1";
+            String query = "select hour(start_time) AS HourOfDay,count(duration) AS TotalDuration from testData group By HourOfDay order By TotalDuration desc limit 1";
             ResultSet rs = st.executeQuery(query);
             // getting the values in rs
             while (rs.next())
@@ -24,7 +24,7 @@ public class Main {
                 System.out.println("Hour of the day when the call volume  highest is "+a+"-"+(a+1)+".");
             }
             //query for Hour of the day when the calls are longest.
-            String query2 = "SELECT HOUR(start_time) AS HourOfDay from testData where duration=(select max(duration) from testData) ";
+            String query2 = "select hour(start_time) AS HourOfDay from testData where duration=(select max(duration) from testData) ";
             ResultSet rs2 = st.executeQuery(query2);
 
             while (rs2.next())
@@ -35,7 +35,7 @@ public class Main {
                 System.out.println("Hour of the day when the call length  highest is "+a+"-"+(a+1)+".");
             }
             //query for Day of the week when the call volume is highest.
-            String query3 = "SELECT dayname(start_time) AS dayOfWeek,count(duration) AS TotalDuration from testData group By dayOfWeek order By TotalDuration desc limit 1";
+            String query3 = "select dayname(start_time) AS dayOfWeek,count(duration) AS TotalDuration from testData group By dayOfWeek order By TotalDuration desc limit 1";
             ResultSet rs3 = st.executeQuery(query3);
             while (rs3.next())
             {
@@ -43,7 +43,7 @@ public class Main {
                 System.out.println("Day of the week when the call volume  highest is " +s);
             }
             //query for Day of the week when the calls are longest.
-            String query4 = "SELECT dayname(start_time) AS dayOfweek from testData where duration=(select max(duration) from testData) ";
+            String query4 = "select dayname(start_time) AS dayOfweek from testData where duration=(select max(duration) from testData) ";
             ResultSet rs4 = st.executeQuery(query4);
             while (rs4.next())
             {
